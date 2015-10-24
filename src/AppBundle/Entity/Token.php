d3b3cf7ffec79223213aa2df9dc5b1361ad28749
Constantin Orasan <dinel@dinel.org.uk>
2015-10-24 23:59:26 +0100

    Added the Token entity

---
+++ src/AppBundle/Entity/Token.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Description of Token
+ *
+ * @author dinel
+ */
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="token")
+ */
+class Token {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=256)
+     */
+    protected $content;
+            
+   /**
+     * @ORM\ManyToOne(targetEntity="Text", inversedBy="tokens")
+     * @ORM\JoinColumn(name="text_id", referencedColumnName="id")
+     */
+    protected $document;
+    
+    /**
+     * The constructor
+     */
+    public function __construct($content) {
+        $this->content = $content;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Token
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set document
+     *
+     * @param \AppBundle\Entity\Text $document
+     *
+     * @return Token
+     */
+    public function setDocument(\AppBundle\Entity\Text $document = null)
+    {
+        $this->document = $document;
+
+        return $this;
+    }
+
+    /**
+     * Get document
+     *
+     * @return \AppBundle\Entity\Text
+     */
+    public function getDocument()
+    {
+        return $this->document;
+    }
+}
