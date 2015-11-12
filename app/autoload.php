553b0be01bcc4ec8357597e4a9d8f0bdaff88870
Constantin Orasan <dinel@dinel.org.uk>
2015-11-12 11:22:43 +0000

    Attempting workaround for an issue with FOS when deploying onto the server

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,9 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+//Loads FOSUserBundle
+$loader->add('FOS', __DIR__.'/../vendor/friendsofsymfony/user-bundle/FOS');
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
