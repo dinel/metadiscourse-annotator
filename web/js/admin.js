bb77682de631e03087364c8cc48ea12a4bc27553
Constantin Orasan <dinel@dinel.org.uk>
2016-01-28 11:11:31 +0000

    Added import and export operations for domains.

--- web/js/admin.js
+++ web/js/admin.js
@@ -6,11 +6,21 @@
 
 
 $( document ).ready(function() {
+  $('#import-domains-form').hide();
+    
   $('.editable-link').mouseenter(function() {
-     $(this).children().last().addClass('fa fa-pencil-square-o'); 
+    $(this).children().last().addClass('fa fa-pencil-square-o'); 
   });
   
   $('.editable-link').mouseleave(function() {
-      $(this).children().last().removeClass('fa fa-pencil-square-o'); 
+    $(this).children().last().removeClass('fa fa-pencil-square-o'); 
+  });
+  
+  $('#import-domains').click(function() {
+    $('#import-domains-form').show();
+  });
+  
+  $('#import-domains-cancel').click(function() {
+    $('#import-domains-form').hide();
   });
 });
