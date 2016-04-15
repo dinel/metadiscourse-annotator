4c651b29b35cca1286081c2dd4d6f3d7539d5ce6
Constantin Orasan <dinel@dinel.org.uk>
2016-04-15 22:51:47 +0100

    Set the text of the button to reflect whether we create a new corpus or edit an existing one

--- src/AppBundle/Form/Type/CorpusType.php
+++ src/AppBundle/Form/Type/CorpusType.php
@@ -17,11 +17,29 @@ use Symfony\Component\Form\FormBuilderInterface;
  * @author dinel
  */
 class CorpusType extends AbstractType {
+    
+    /** 
+     * The id of the corpus to be edited. Currently the value is not used
+     * only if it is null or not
+     * @var type 
+     */
+    private $id;
+
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('name', 'text')
-            ->add('description', 'text')        
-            ->add('save', 'submit', array('label' => 'Add corpus'));
+            ->add('description', 'textarea');
+        
+        if($this->id) {
+            $builder->add('save', 'submit', array('label' => 'Edit corpus'));
+        } else {
+            $builder->add('save', 'submit', array('label' => 'Add corpus'));
+        }
     }
         
     public function getName() {
