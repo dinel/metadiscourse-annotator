18145aac0d0ba520c46a82e301ac0afbe2bc395f
Constantin Orasan <dinel@dinel.org.uk>
2017-02-23 15:15:20 +0000

    Added the option to select categories for a sense.

--- src/AppBundle/Form/Type/SenseType.php
+++ src/AppBundle/Form/Type/SenseType.php
@@ -1,9 +1,19 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 - 2017 dinel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace AppBundle\Form\Type;
@@ -16,6 +26,7 @@ use Symfony\Component\Form\FormBuilderInterface;
  *
  * @author dinel
  */
+
 class SenseType extends AbstractType {
     private $in_edit_mode = false;
 
@@ -35,6 +46,16 @@ class SenseType extends AbstractType {
                 ))
             ->add('score', 'text', array(
                     'label' => 'Default score:',
+                ))
+            ->add('categories', 'entity', array(
+                    'class'     => 'AppBundle:Category',
+                    'choice_label' => 'Categories',
+                    'expanded'  => true,
+                    'multiple'  => true,
+                    'query_builder' => function(\Doctrine\ORM\EntityRepository $er) {
+                            return $er->createQueryBuilder('d')
+                                      ->where('d.parent IS NOT NULL');
+                    },
                 ))
             ->add('fgColor', 'text', array(
                     'label' => 'Text color:',
