43b8576183e1ec7e9e5354fda7ee8bedda7bec08
Constantin Orasan <dinel@dinel.org.uk>
2018-01-31 19:30:54 +0000

    Updated license information

--- src/AppBundle/Form/Type/SenseType.php
+++ src/AppBundle/Form/Type/SenseType.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2015 - 2017 dinel.
+ * Copyright 2015 - 2018 dinel.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
