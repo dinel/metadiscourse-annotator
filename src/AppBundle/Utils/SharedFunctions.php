9193e2cdb930c37e9c2f17a82e969ffc52e77365
Constantin Orasan <dinel@dinel.org.uk>
2018-02-08 23:50:19 +0000

    Moved here deleteMarkable and deleteSense

--- src/AppBundle/Utils/SharedFunctions.php
+++ src/AppBundle/Utils/SharedFunctions.php
@@ -62,6 +62,80 @@ class SharedFunctions {
         
         return $cat_tree;
     }
+        
+    /**
+     * Removes a markable from the database and all the annotation associated
+     * with it. Takes an optional parameter which indicates which form
+     * @param Markable $mark
+     * @param type $em
+     * @param $doctrine handle to doctrine
+     * @param string $form the alternative form to delete
+     */
+    public static function removeMarkable($mark, $em, $doctrine, $form = null) {
+        $tokens = $doctrine->getRepository('\AppBundle\Entity\Token')
+                           ->createQueryBuilder('t')
+                           ->where('t.markable = :id')
+                           ->setParameter('id', $mark->getId())
+                           ->getQuery()
+                           ->iterate();
+            
+        while (($row = $tokens->next()) !== false) {            
+            $token = $row[0];
+            if($form && ! SharedFunctions::sameWord($form, $token->getContent())) {
+                continue;
+            }
+            
+            $token->setMarkable(null);
+            $em->persist($token);
+            
+            // tidy up if there is any left annotation with senseid = 0 (not metadiscourse)
+            $annotations = $doctrine->getRepository('AppBundle:Annotation')
+                                    ->createQueryBuilder('a')
+                                    ->where('a.token = :id')
+                                    ->setParameter('id', $token->getId())
+                                    ->getQuery()
+                                    ->iterate();
+            while (($row = $annotations->next()) !== false) {
+                $annotation = $row[0];
+                $em->remove($annotation);
+            }
+        }
+        if(! $em->contains($mark)) {
+            $mark = $em->merge($mark);
+        }
+
+        $em->flush();
+        if(! $form) $em->remove($mark);
+        $em->flush();
+        $em->clear();
+    }
+    
+    /**
+     * Removes a sense
+     * @param AppBundle\Entity\Sense $sense
+     * @param AppBundle\Entity\Markable $mark
+     * @param type $em
+     */
+    public static function removeSense($sense, $mark, $em, $doctrine) {
+        $annotations = $doctrine->getRepository('AppBundle:Annotation')
+                                ->createQueryBuilder('a')
+                                ->where('a.sense = :id')
+                                ->setParameter('id', $sense->getId())
+                                ->getQuery()
+                                ->iterate();
+            
+        while (($row = $annotations->next()) !== false) {
+            $annotation = $row[0];
+            $em->remove($annotation);
+        }
+        $mark->removeSense($sense);
+        if(! $em->contains($sense)) {
+            $sense = $em->merge($sense);
+        }
+        $em->remove($sense);
+        $em->flush();
+        $em->clear();
+    }    
     
     /**
      * Function which compares two strings
@@ -72,4 +146,5 @@ class SharedFunctions {
     public static function sameWord($a, $b) {
         return strtolower($a) == strtolower($b);
     }
+
 }
