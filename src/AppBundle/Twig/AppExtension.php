967c1b15c10aeb1a9611ae08c5fb2f6578892019
Constantin Orasan <c.orasan@wlv.ac.uk>
2018-03-07 13:48:19 +0000

    Added licence information

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -1,18 +1,29 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 - 2017 dinel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Twig extension which returns the hash for a marker
+ *
+ * @author dinel
  */
 
 namespace AppBundle\Twig;
 
-/**
- * Description of AppExtension
- *
- * @author dinel
- */
 class AppExtension extends \Twig_Extension {
     public function getFilters()
     {
@@ -42,8 +53,3 @@ class AppExtension extends \Twig_Extension {
         return 'app_extension';
     }
 }
-
-
-
-
-
