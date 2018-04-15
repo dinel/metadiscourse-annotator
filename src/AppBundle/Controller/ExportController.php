3ecfc1a84f1f012e31374e02d37e9f86a3926b7e
Constantin Orasan <dinel@dinel.org.uk>
2018-04-15 22:42:57 +0100

    Initial commit

--- src/AppBundle/Controller/ExportController.php
+++ src/AppBundle/Controller/ExportController.php
@@ -18,11 +18,79 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use AppBundle\Utils\SharedFunctions;
+
 /**
  * Description of ExportController
  *
  * @author dinel
  */
-class ExportController {
-    //put your code here
+class ExportController extends Controller {
+    
+    /**
+     * Action which exports a text as XML
+     * @Route("/admin/export_document/{id}", name="export_document")
+     * @Method({"GET"})
+     * @param integer $id the ID of the document to be exported
+     */
+    public function exportToXMLAction($id) {
+        $this->getDoctrine()->getEntityManager()->getConnection()->getConfiguration()->setSQLLogger(null);
+        ini_set('memory_limit', '-1');
+        
+        $annotated_doc = "<doc>";
+        $doc = $this->getDoctrine()
+                ->getRepository('AppBundle:Text')
+                ->find($id);
+        
+        if($doc) {            
+            $tokens = SharedFunctions::getTokensFromDocument($id, 
+                    $this->getDoctrine());
+            
+            while (($row = $tokens->next()) !== false) {
+                $token = $row[0];
+                $annotations = $this->getAnnotation($token->getId());
+                $element = htmlspecialchars($token->getContent());
+                 
+                if($annotations) {
+                    $tags = "<marker id='" . $token->getId() . "'>";
+                    foreach($annotations as $annotation) {
+                        $tags .= sprintf("<annotation id='%s' annotator='%s' sense='%s'/>",
+                                $annotation->getId(), 
+                                $annotation->getUserName(),
+                                $annotation->getSense() ? $annotation->getSense()->getDefinition() : "NOT");
+                    }
+                    $element = $tags . $element . "</marker>";
+                } else {
+                    $element = " " . $element . " ";
+                }
+                
+                if($token->getNewLineBefore()) {
+                    $element .= "\n";
+                }
+                $annotated_doc .= $element;
+            }
+        }        
+        
+        $annotated_doc .= "</doc>";
+        $response = new Response($annotated_doc);
+        $response->headers->set('Content-Type', 'application/xml; charset=utf-8');
+        return $response;
+    }
+    
+    /**
+     * Returns the annotation for a token
+     * @param integer $token
+     * @return array an array which contains the annotation assigned to a token
+     */
+    private function getAnnotation($token) {
+        return $this->getDoctrine()
+                    ->getRepository('AppBundle:Annotation')
+                    ->findBy(array('token' => $token));
+    }
 }
