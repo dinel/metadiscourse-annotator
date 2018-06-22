2380f44afd36de689d268e22364c1e0b134f7c0d
Constantin Orasan <c.orasan@wlv.ac.uk>
2018-06-22 11:15:50 +0100

    Display details about the concordance

--- web/js/concordancer.js
+++ web/js/concordancer.js
@@ -15,6 +15,44 @@
  */
 
 $( document ).ready(function() {
+    $("#results").on('click', '.details', function(e) {          
+        var node = $(this).next();
+        console.log(node);
+        if(node.html()) {
+            node.parent().toggle();
+            return;
+        }
+        
+        $.ajax({
+            type: 'GET',
+            url: '/search/retrieve_info/' + $(this).attr("id"),
+            success: function(data) {
+                var display = "<strong>Annotator: </strong>" + data.annotator;
+                display += "&nbsp;&nbsp;|&nbsp;&nbsp;<strong>Sense selected: </strong> " + data.sense;
+                display += "&nbsp;&nbsp;|&nbsp;&nbsp;<strong>Category: </strong> " + data.category;
+                display += "&nbsp;&nbsp;|&nbsp;&nbsp;<strong>Polarity: </strong> " + data.polarity;
+                display += "<br/><strong>Notes: </strong>" + data.comments;
+                display += "<br/><strong>Source: </strong>" + data.source;
+                display += "<span style='margin-left: 5em'><a target='_blank' " 
+                        + " href='/document/" + data.id_document + "/" + data.id_token + "'"
+                        + ">Go to annotation</a></span>";
+                if(data.source) {
+                    display += "<br/><strong>Source segment: </strong>" + data.source;
+                    display += "<br/><strong>Target segment: </strong>" + data.target;
+                }
+                
+                if(data.uncertain === true) {
+                    display += "<br/><strong style='text-color: red'>The annotator was uncertain about this annotation!!</strong>";
+                }
+                display += "<br/>";                
+                node.html(display);
+            }
+        });
+                e.preventDefault();
+        
+        node.parent().show();
+    }); 
+    
     $('.left-context').each(function(index) {
         trimText($(this).children().get(0), 0);
     });
